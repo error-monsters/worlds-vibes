7b76f05935803ca08b91b86aaff0ed470121edc3
omar4 <tarawri.dev@gmail.com>
2019-07-08 17:48:00 +0300

    API country work

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import { Route, Link } from 'react-router-dom' 
 // import logo from './logo.svg';
 import Country from './Country' 
-import CountryInfo from './CountryInfo' 
+// import CountryInfo from './CountryInfo' 
 // import Music from './Music' 
 // import Images from './Images' 
 import Nav from './Nav' 
@@ -18,36 +18,40 @@ class App extends Component {
  
   render() {
     return ( 
-      <div>  
-        <Nav/>
-        <Country /> 
+      <React.Fragment>  
+        
         <Route exact path='/' render={()=>{
           return (
-            <div className ='home-bg'>   
-              <video autoPlay loop muted >
-                <source src={vid} type="video/mp4" />
-              </video> 
-              <Link className = 'virtical' to='/Explore'>Explore The World  </Link> 
-              <Link to='/Explore'>
-              <img id="small-img" src={earth1} alt ='earth pic'/> 
-              </Link>
+            <div class='home-route'> 
+              <Nav/> 
+              <div className ='home-bg'>   
+                <video autoPlay loop muted >
+                  <source src={vid} type="video/mp4" />
+                </video> 
+                <Link className = 'virtical' to='/Explore'>Explore The World  </Link> 
+                <Link to='/Explore'>
+                <img id="small-img" src={earth1} alt ='earth pic'/> 
+                </Link>
+              </div>
             </div>
           )
         }}
         />
-        <CountryInfo />
+       
         <Route path='/Explore' component={Explore}/>
        
         <Route path='/Country/:id' render={(props)=>{
           const countryName = props.match.params.id
           // <Country countryName={countryName} />
           return (
-            
-            <p>{countryName}</p>
+            <div class='country-route'> 
+              <Nav/> 
+              <Country countryName={countryName} />
+            </div>
           )
         }}
         /> 
-      </div> 
+      </React.Fragment> 
     )
   }
 }
--- src/markers.js
+++ src/markers.js
@@ -2,27 +2,24 @@ export default [
   {
     id: 1,
     city: 'Singapore',
+    country: 'Singapore',
     color: 'red',
     coordinates: [1.3521, 103.8198],
     value: 50,
   },
   {
     id: 2,
-    city: 'New York',
+    city: 'Washington, D.C.',
+    country: 'United States of America',
     color: 'blue',
-    coordinates: [40.73061, -73.935242],
+    coordinates: [38.89511,-77.03637],
     value: 25,
   },
-  {
-    id: 3,
-    city: 'San Francisco',
-    color: 'orange',
-    coordinates: [37.773972, -122.431297],
-    value: 35,
-  },
+  
   {
     id: 4,
     city: 'Beijing',
+    country: 'China',
     color: 'gold',
     coordinates: [39.9042, 116.4074],
     value: 0,
@@ -30,14 +27,92 @@ export default [
   {
     id: 5,
     city: 'London',
+    country: 'United Kingdom of Great Britain and Northern Ireland',
     color: 'black',
     coordinates: [51.5074, 0.1278],
     value: 80,
   },{
     id: 6,
-    city: 'Jeddah', 
+    city: 'Riyadh', 
+    country: 'Saudi Arabia',
     color: 'green',
     coordinates: [24.266906, 45.107849],
     value: 100,
-  }
+  },
+
+
+
+
+
+
+
+
+
+
+  {
+    id: 7,
+    city: 'Beijing',
+    country: 'China',
+    color: 'red',
+    coordinates: [39.913818, 116.363625],
+    value: 50,
+  },
+
+  {
+    id: 7,
+    city: 'Cairo',
+    country: 'Egypt',
+    color: 'red',
+    coordinates: [30.044281, 31.340002],
+    value: 50,
+  },
+
+
+  {
+    id: 7,
+    city: 'Moscow',
+    country: 'Russia',
+    color: 'red',
+    coordinates: [55.751244, 37.618423],
+    value: 50,
+  },
+
+
+  {
+    id: 7,
+    city: 'Brasilia',
+    country: 'Belize',
+    color: 'red',
+    coordinates: [-15.699244, 47.829556],
+    value: 50,
+  },
+
+  {
+    id: 7,
+    city: 'Tokyo',
+    country: 'Japan',
+    color: 'red',
+    coordinates: [35.652832, 139.839478],
+    value: 50,
+  },
+
+  {
+    id: 7,
+    city: 'Sydney',
+    country: 'Australia',
+    color: 'red',
+    coordinates: [-33.865143, 151.209900],
+    value: 50,
+  },
+
+  {
+    id: 7,
+    city: 'Cape Town',
+    country: 'South Africa',
+    color: 'red',
+    coordinates: [-33.918861, 18.423300],
+    value: 50,
+  },
+
 ];
+
