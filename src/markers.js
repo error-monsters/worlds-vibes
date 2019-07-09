84396ea43a88f5c753da6436b1ba730314eb305f
omar4 <tarawri.dev@gmail.com>
2019-07-09 14:52:15 +0300

    add new countries in markers file

--- src/markers.js
+++ src/markers.js
@@ -5,65 +5,54 @@ export default [
     country: 'Singapore',
     color: 'red',
     coordinates: [1.3521, 103.8198],
+    music: 'https://www.lengua.com/anthems/Singapur.mp3',
     value: 50,
   },
   {
     id: 2,
     city: 'Washington, D.C.',
     country: 'United States of America',
-    color: 'blue',
+    color: 'orange',
     coordinates: [38.89511,-77.03637],
+    music: 'https://www.lengua.com/anthems/USA.mp3',
     value: 25,
   },
   
   {
-    id: 4,
+    id: 3,
     city: 'Beijing',
     country: 'China',
-    color: 'gold',
+    color: 'yellow',
     coordinates: [39.9042, 116.4074],
+    music: 'https://www.lengua.com/anthems/China.mp3',
     value: 0,
   },
   {
-    id: 5,
+    id: 4,
     city: 'London',
     country: 'United Kingdom of Great Britain and Northern Ireland',
-    color: 'black',
+    color: 'blue',
     coordinates: [51.5074, 0.1278],
+    music: '',
     value: 80,
   },{
-    id: 6,
+    id: 5,
     city: 'Riyadh', 
     country: 'Saudi Arabia',
     color: 'green',
     coordinates: [24.266906, 45.107849],
+    music: 'https://www.lengua.com/anthems/SaudiArabia.mp3',
     value: 100,
   },
 
 
-
-
-
-
-
-
-
-
   {
-    id: 7,
-    city: 'Beijing',
-    country: 'China',
-    color: 'red',
-    coordinates: [39.913818, 116.363625],
-    value: 50,
-  },
-
-  {
-    id: 7,
+    id: 6,
     city: 'Cairo',
     country: 'Egypt',
-    color: 'red',
+    color: 'black',
     coordinates: [30.044281, 31.340002],
+    music: 'https://www.lengua.com/anthems/Egypt.mp3',
     value: 50,
   },
 
@@ -72,47 +61,145 @@ export default [
     id: 7,
     city: 'Moscow',
     country: 'Russia',
-    color: 'red',
+    color: 'white',
     coordinates: [55.751244, 37.618423],
+    music: 'https://www.lengua.com/anthems/Russia.mp3',
     value: 50,
   },
 
 
   {
-    id: 7,
+    id: 8,
     city: 'Brasilia',
-    country: 'Belize',
-    color: 'red',
-    coordinates: [-15.699244, 47.829556],
+    country: 'Brazil',
+    color: 'gold',
+    coordinates: [-15.7745457,-48.3575684],
+    music: 'https://www.lengua.com/anthems/Brazil.mp3',
     value: 50,
   },
 
   {
-    id: 7,
+    id: 9,
     city: 'Tokyo',
     country: 'Japan',
     color: 'red',
     coordinates: [35.652832, 139.839478],
+    music: 'https://www.lengua.com/anthems/Japan.mp3',
     value: 50,
   },
 
   {
-    id: 7,
+    id: 10,
     city: 'Sydney',
     country: 'Australia',
-    color: 'red',
+    color: 'blue',
     coordinates: [-33.865143, 151.209900],
+    music: 'https://www.lengua.com/anthems/Australia.mp3',
     value: 50,
   },
 
   {
-    id: 7,
+    id: 11,
     city: 'Cape Town',
     country: 'South Africa',
-    color: 'red',
+    color: 'gold',
     coordinates: [-33.918861, 18.423300],
+    music: 'https://www.lengua.com/anthems/South_africa.mp3',
     value: 50,
   },
+
+  {
+    id: 12,
+    city: 'New Delhi',
+    country: 'India',
+    color: 'orange',
+    coordinates: [28.644800,  77.216721],
+    music: 'https://www.lengua.com/anthems/India.mp3',
+    value: 33,
+  },
+
+  {
+    id: 13,
+    city: 'Berlin',
+    country: 'Germany',
+    color: 'red',
+    coordinates: [52.520008, 13.404954],
+    music: 'https://www.lengua.com/anthems/Germany.mp3',
+    value: 43,
+  },
+
+  {
+    id: 14,
+    city: 'Madrid',
+    country: 'Spain',
+    color: 'eellow',
+    coordinates: [40.416775, -3.703790],
+    music: 'https://www.lengua.com/anthems/Spain.mp3',
+    value: 43,
+  },
+
+  {
+    id: 15,
+    city: 'Athens',
+    country: 'Greece',
+    color: 'blue',
+    coordinates: [37.983810, 23.727539],
+    music: 'https://www.lengua.com/anthems/Greece.mp3',
+    value: 43,
+  },
+
+  {
+    id: 16,
+    city: 'Beirut',
+    country: 'Lebanon',
+    color: 'red',
+    coordinates: [33.888630, 35.495480],
+    music: 'https://www.lengua.com/anthems/Lebanon.mp3',
+    value: 43,
+  },
+
+  {
+    id: 17,
+    city: 'Havana',
+    country: 'Cuba',
+    color: 'blue',
+    coordinates: [23.113592, -82.366592],
+    music: 'https://www.lengua.com/anthems/Cuba.mp3',
+    value: 43,
+  },
+
+  {
+    id: 18,
+    city: 'Bamako',
+    country: 'Mali',
+    color: 'green',
+    coordinates: [12.6125547,-8.1355996],
+    music: '',
+    value: 43,
+  },
+
+  {
+    id: 20,
+    city: 'Kinshasa',
+    country: 'Democratic Republic of the Congo',
+    color: 'gole',
+    coordinates: [-4.4012906,15.1826578],
+    music: 'https://www.lengua.com/anthems/Congo.mp3',
+    value: 43,
+  },
+
+  {
+    id: 21,
+    city: 'Bogotá',
+    country: 'Colombia',
+    color: 'white',
+    coordinates: [4.6482837,-74.2478938],
+    music: 'https://www.lengua.com/anthems/Colombia.mp3',
+    value: 43,
+  },
+
+ 
+
 
 ];
 
