8ac371c5ad8aecb966a4414fbcf4550ecd88c8f6
omar4 <tarawri.dev@gmail.com>
2019-07-09 18:49:40 +0300

    add about component

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React,{ Component} from "react"
+import Nav from './Nav'
+import './About.css'
+class About extends Component{
+    render (){
+        return ( 
+            
+        <div className ='about-page'>
+            <Nav />
+            <h1>hi</h1>
+        </div>)
+    }
+}
+export default About;
--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import './App.css'
 import earth1 from "./earth1.png"
 import vid from './vid.mp4';
 
+import About from './About'
+
 
 
 class App extends Component {
@@ -37,7 +39,7 @@ class App extends Component {
         />
        
         <Route path='/Explore' component={Explore}/>
-       
+
         <Route path='/Country/:id' render={(props)=>{
           const countryName = props.match.params.id
           // <Country countryName={countryName} />
@@ -49,6 +51,8 @@ class App extends Component {
           )
         }}
         /> 
+
+      <Route path='/About' component={About}/> 
       </React.Fragment> 
     )
   }
