366091563884bd91eb302bd77d119f813e04b868
hudabz <huda_bz@hotmail.com>
2019-07-10 17:23:10 +0300

    Updates

---
+++ precache-manifest.ce208947943ed53ab6172da39c986e30.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6218271a9196ef142373",
+    "url": "/worlds-vibes/static/css/main.3a56c5b9.chunk.css"
+  },
+  {
+    "revision": "6218271a9196ef142373",
+    "url": "/worlds-vibes/static/js/main.f2a7ff84.chunk.js"
+  },
+  {
+    "revision": "0dc8b02a081abd8c0206",
+    "url": "/worlds-vibes/static/js/runtime~main.fc33b26f.js"
+  },
+  {
+    "revision": "4b7f4fd2ad0c37cbc908",
+    "url": "/worlds-vibes/static/js/2.430ddb10.chunk.js"
+  },
+  {
+    "revision": "3f0ef89791b0456b40868d1f48dc4958",
+    "url": "/worlds-vibes/static/media/about_us.3f0ef897.jpeg"
+  },
+  {
+    "revision": "935032824d7615ab875b4c71d5bc2217",
+    "url": "/worlds-vibes/static/media/earth1.93503282.png"
+  },
+  {
+    "revision": "bc44b53f84bf3680c5ce610acfe11585",
+    "url": "/worlds-vibes/static/media/vid.bc44b53f.mp4"
+  },
+  {
+    "revision": "e06c757045f36e5f5a97dd692db2ad8b",
+    "url": "/worlds-vibes/static/media/2.e06c7570.png"
+  },
+  {
+    "revision": "299b71d0178d1ee5850d7115f17de8b1",
+    "url": "/worlds-vibes/static/media/3.299b71d0.png"
+  },
+  {
+    "revision": "a96f5217f622368f3633d5200fd81fd2",
+    "url": "/worlds-vibes/static/media/4.a96f5217.jpg"
+  },
+  {
+    "revision": "2d81f0a5baa6c1c868e57ecb8a56cb5a",
+    "url": "/worlds-vibes/index.html"
+  }
+];
